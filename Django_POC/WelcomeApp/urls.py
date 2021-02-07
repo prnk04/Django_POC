1e5678ab75e28ec1074d253aa16a7a53c9add1ae
Priyanka Pandey <prnkpandey00@gmail.com>
2021-02-07 23:31:26 +0530

    added links and removed extra stuff
    added hyperlink for images, videos and notes
    removed the code that was taking user input

--- Django_POC/WelcomeApp/urls.py
+++ Django_POC/WelcomeApp/urls.py
@@ -4,8 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.homePage, name = "homePage"),
-    path('index/', views.homePage, name = "homePage"), # we can remove this line if you want
-    path('add/' , views.addContent, name = 'contribute'),
     path('<str:queryName>/', views.findPage, name = "findPage")
     
 ]
